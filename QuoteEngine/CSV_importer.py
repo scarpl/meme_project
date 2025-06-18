b9fbb7c38a55c6da1a84d6168fa39a5a8171316b
Luca <lucascarpantonio@MacLuca.local>
2025-06-18 09:30:14 +0200

    Created CSV importer

---
+++ QuoteEngine/CSV_importer.py
@@ -0,0 +1,27 @@
+
+from typing import List
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+import pandas
+
+
+class CSVImporter(IngestorInterface):
+    """Module to read CSV file."""
+
+    allowed_extensions = ['csv']
+
+    @classmethod
+    def parse(cls, path: str):
+        """Parse CSV file and list of quote models."""
+        if not cls.can_ingest(path):
+            raise Exception('Connot Ingest Exception')
+
+        quotes = []
+
+        df = pandas.read_csv(path, header=0)
+
+        for index, row in df.iterrows():
+            new_quote = QuoteModel(row['body'], row['author'])
+            quotes.append(new_quote)
+
+        return quotes
