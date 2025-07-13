ae0b76d3e8cb4e4ae4249cc4aafd4f8a48e6fe18
Luca Scarpantonio <lucascarpantonio@gmail.com>
2025-07-13 17:45:55 +0200

    Checked pycode and pydocstyle

--- MemeEngine.py
+++ MemeEngine.py
@@ -3,6 +3,8 @@
 import os
 import random
 from PIL import Image, ImageDraw, ImageFont
+from QuoteEngine import Ingestor
+import argparse
 
 
 class MemeCreator:
@@ -64,3 +66,68 @@ class MemeCreator:
             raise ValueError(f"Cannot open or process image: {path}")
         except Exception as e:
             raise RuntimeError(f"Unexpected error during meme creation: {e}")
+
+
+def build_meme(path=None, body=None, author=None):
+    """
+    Create a meme by combining a quote and an image.
+
+    Parameters:
+    - path (str): path to the image file
+    - body (str): quote body
+    - author (str): quote author
+
+    Returns:
+    - str: path to the generated meme image
+    """
+    # Load image
+    if path:
+        image = path
+    else:
+        images = "./_data/photos/dog"
+        imgs = [
+            os.path.join(root, file)
+            for root, _, files in os.walk(images)
+            for file in files if file.endswith(('.jpg', '.png'))
+        ]
+        image = random.choice(imgs)
+
+    # Load quote
+    if body and author:
+        quote = QuoteModel(body, author)
+    elif body and not author:
+        raise ValueError("Author is required if quote text is provided.")
+    else:
+        quote_file = [
+            "./_data/DogQuotes/DogQuotesTXT.txt",
+            "./_data/DogQuotes/DogQuotesDOCX.docx",
+            "./_data/DogQuotes/DogQuotesPDF.pdf",
+            "./_data/DogQuotes/DogQuotesCSV.csv"
+        ]
+        quotes = []
+        for f in quote_file:
+            quotes.extend(Ingestor.parse(f))
+
+        quote = random.choice(quotes)
+
+    meme_generator = MemeCreator("./tmp")
+    meme_path = meme_generator.create(image, quote.body, quote.author)
+
+    return meme_path
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Meme Generator CLI")
+    parser.add_argument('--body', type=str,
+                        help="Quote text to add to the meme")
+    parser.add_argument('--author', type=str, help="Author of the quote")
+    parser.add_argument('--path', type=str,
+                        help="Path to the background image")
+
+    args = parser.parse_args()
+
+    try:
+        result = build_meme(args.path, args.body, args.author)
+        print(f"Meme successfully created at: {result}")
+    except Exception as e:
+        print(f"Error: {e}")
